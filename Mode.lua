2f6d8bbdf0ad92f9bd70a50a40e0476ac2d49c57
Rebound <eliazbp92@gmail.com>
2026-08-21 17:37:43 -0700

    Update Mode.lua

--- Mode.lua
+++ Mode.lua
@@ -2,6 +2,7 @@
 coroutine.wrap(function()
     while true do
         wait(0)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/eliazbp92-collab/Mayhem-mode/refs/heads/main/Credit"))()
     end
 end)()
